1f568d07fba65279bd755991bf4a2a08c9591669
jasonchilcott <jasonchilcottcode@gmail.com>
2020-10-08 15:56:43 -0400

    first two deliverables

--- src/index.js
+++ src/index.js
@@ -12,4 +12,99 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+    const url = "http://localhost:3000/toys/"
+    
+    
+    const renderToys = toys => {
+      for(const toyObj of toys){
+        renderToy(toyObj)
+      }
+    }
+
+
+    function fetchToys() {
+      fetch(url)
+      .then(response => response.json())
+      .then(toys => renderToys(toys))
+    }
+
+
+    
+    function renderToy(toyObj) {
+      let toyCard = document.createElement("div")
+      let toyCollection = document.getElementById('toy-collection')
+      toyCard.setAttribute('class', 'card')
+      toyCard.innerHTML = `
+        <h2>${toyObj.name}</h2>
+        <img src="${toyObj.image}" class="toy-avatar" />
+        <p>${toyObj.likes} likes</p>
+        <button class="like-btn">Like <3</button>
+      `
+      toyCollection.append(toyCard)
+    }
+
+
+    
+    function submitHandler() {
+      const formDiv = document.querySelector('.add-toy-form')
+      formDiv.addEventListener('submit', function(e) {
+        e.preventDefault()
+        
+        let toyPost = {
+          name: formDiv["name"].value, 
+          image:  formDiv["image"].value,
+          likes: 0 }
+          
+          // debugger
+      const options = {
+          method: 'POST',
+          headers: {
+            "content-type": "application/json",
+            "accept": "application/json"
+          },
+          body: JSON.stringify(toyPost)
+        }
+
+        fetch(url, options)
+        .then(response => response.json())
+        .then(toy => renderToy(toy))
+
+    })
+
+  }
+
+
+
+
+    fetchToys()
+    // postToy()
+    submitHandler()
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
+
+//1: Make a get request to Andy's Toys (fetch) ---
+//2: Get the data from the fetch request ---
+//3: Create card element with Data from fetch request --
+//4: Make post request to database 
+//5: Make patch request to database
